32263160c1483c0f9968fe7c70eef4c3c47fb21c
Yamine BULACH <contact@yamine-bulach.eu>
2016-02-14 15:33:26 +0100

    Added LGPL license

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# This file is part of pysshrp.
+#
+# pysshrp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pysshrp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pysshrp.  If not, see <http://www.gnu.org/licenses/>.
 
 try:
 	from setuptools import setup
@@ -10,6 +25,7 @@ setup(
 	name = 'pysshrp',
 	description = 'A Python SSH/SFTP reverse proxy',
 	version = '0.1',
+	license = 'LGPL'
 	author = 'Yamine BULACH',
 	author_email = 'contact@yamine-bulach.eu',
 	url = 'https://github.com/ybulach/pysshrp',
