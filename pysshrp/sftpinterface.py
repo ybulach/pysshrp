d200a2e763a260ee335b9676e28e68a432946f76
Yamine BULACH <contact@yamine-bulach.eu>
2016-02-22 16:42:59 +0100

    Fixed security issues with upstream_path

--- pysshrp/sftpinterface.py
+++ pysshrp/sftpinterface.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pysshrp.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, paramiko
+import errno, os, paramiko
 
 class SFTPInterface(paramiko.SFTPServerInterface):
 	def __init__(self, server, *largs, **kwargs):
@@ -25,28 +25,34 @@ class SFTPInterface(paramiko.SFTPServerInterface):
 		self.client = paramiko.SFTPClient.from_transport(server.client)
 		self.root_path = server.root_path if ('root_path' in server.__dict__) else ''
 
+	def _parsePath(self, path):
+		result = os.path.normpath(self.root_path + '/' + path)
+		if not result.startswith(self.root_path):
+			raise IOError(errno.EACCES)
+		return result
+
 	def list_folder(self, path):
 		try:
-			return self.client.listdir_attr(self.root_path + path)
+			return self.client.listdir_attr(self._parsePath(path))
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 
 	def stat(self, path):
 		try:
-			return self.client.stat(self.root_path + path)
+			return self.client.stat(self._parsePath(path))
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 
 	def lstat(self, path):
 		try:
-			return self.client.lstat(self.root_path + path)
+			return self.client.lstat(self._parsePath(path))
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 
 	def open(self, path, flags, attr):
 		if (flags & os.O_CREAT) and (attr is not None):
 			attr._flags &= ~attr.FLAG_PERMISSIONS
-			paramiko.SFTPServer.set_file_attr(self.root_path + path, attr)
+			paramiko.SFTPServer.set_file_attr(self._parsePath(path), attr)
 		if flags & os.O_WRONLY:
 			if flags & os.O_APPEND:
 				fstr = 'ab'
@@ -62,12 +68,12 @@ class SFTPInterface(paramiko.SFTPServerInterface):
 			fstr = 'rb'
 
 		try:
-			f = self.client.open(self.root_path + path, fstr)
+			f = self.client.open(self._parsePath(path), fstr)
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 
 		fobj = paramiko.SFTPHandle(flags)
-		fobj.filename = self.root_path + path
+		fobj.filename = self._parsePath(path)
 		fobj.readfile = f
 		fobj.writefile = f
 		fobj.client = self.client
@@ -76,14 +82,14 @@ class SFTPInterface(paramiko.SFTPServerInterface):
 
 	def remove(self, path):
 		try:
-			self.client.remove(self.root_path + path)
+			self.client.remove(self._parsePath(path))
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 		return paramiko.SFTP_OK
 
 	def rename(self, oldpath, newpath):
 		try:
-			self.client.rename(self.root_path + oldpath, self.root_path + newpath)
+			self.client.rename(self._parsePath(oldpath), self._parsePath(newpath))
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 		return paramiko.SFTP_OK
@@ -91,16 +97,16 @@ class SFTPInterface(paramiko.SFTPServerInterface):
 	def mkdir(self, path, attr):
 		try:
 			if attr.st_mode is None:
-				self.client.mkdir(self.root_path + path)
+				self.client.mkdir(self._parsePath(path))
 			else:
-				self.client.mkdir(self.root_path + path, attr.st_mode)
+				self.client.mkdir(self._parsePath(path), attr.st_mode)
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 		return paramiko.SFTP_OK
 
 	def rmdir(self, path):
 		try:
-			self.client.rmdir(self.root_path + path)
+			self.client.rmdir(self._parsePath(path))
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
 		return paramiko.SFTP_OK
@@ -108,13 +114,13 @@ class SFTPInterface(paramiko.SFTPServerInterface):
 	def chattr(self, path, attr):
 		try:
 			if attr._flags & attr.FLAG_PERMISSIONS:
-				self.client.chmod(self.root_path + path, attr.st_mode)
+				self.client.chmod(self._parsePath(path), attr.st_mode)
 			if attr._flags & attr.FLAG_UIDGID:
-				self.client.chown(self.root_path + path, attr.st_uid, attr.st_gid)
+				self.client.chown(self._parsePath(path), attr.st_uid, attr.st_gid)
 			if attr._flags & attr.FLAG_AMTIME:
-				self.client.utime(self.root_path + path, (attr.st_atime, attr.st_mtime))
+				self.client.utime(self._parsePath(path), (attr.st_atime, attr.st_mtime))
 			if attr._flags & attr.FLAG_SIZE:
-				with self.client.open(self.root_path + path, 'w+') as f:
+				with self.client.open(self._parsePath(path), 'w+') as f:
 					f.truncate(attr.st_size)
 		except IOError as e:
 			return paramiko.SFTPServer.convert_errno(e.errno)
